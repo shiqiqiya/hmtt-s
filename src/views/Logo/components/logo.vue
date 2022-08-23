f59076aac150661206418215b2393e57131da440
shiqiqi <2418687931@qq.com>
2022-08-23 22:38:12 +0800

    测试一下

--- src/views/Logo/components/logo.vue
+++ src/views/Logo/components/logo.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>这还是一个用来测试分支的</div>
+  <div>
+    这还是一个用来测试分支的
+    <van-nav-bar title="登录" left-arrow @click-left="$router.back()">
+      <template #left>
+        <van-icon name="cross"></van-icon>
+      </template>
+    </van-nav-bar>
+  </div>
 </template>
 
 <script>
